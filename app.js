fb78dd6484ba3f9d8a54baf639a0bf027ffe41f6
Alkan Orgun <alkanorgun@gmail.com>
2020-11-08 10:57:51 +0300

    Player Factory implementation and init of Game Board & Display Conroller modules

---
+++ app.js
@@ -0,0 +1,21 @@
+// Gameboard Module
+const gameBoard = (function() {
+  'use strict';
+
+  let cells = [];
+
+  
+})
+
+// Player Factory
+const playerFactory = (name, color) => {
+  const getName = () => name;
+  const getColor = () => color;
+  return { getName, getColor };
+}
+
+
+// DisplayController Module
+const displayController = (function () {
+  'use strict';
+})
